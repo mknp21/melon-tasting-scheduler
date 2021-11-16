7e14d6140624256031c6627e027528bbb55ba2e1
Mika Patterson <mknpatt21@gmail.com>
2021-11-16 02:27:05 +0000

    Imported faker

--- seed_db.py
+++ seed_db.py
@@ -1,1 +1,15 @@
 """Drop db, create db, and automatically populate db with data."""
+
+import crud
+import model
+# import server
+
+from faker import Faker
+fake = Faker()
+
+os.system('dropdb rezzies')
+os.system('createdb rezzies')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
