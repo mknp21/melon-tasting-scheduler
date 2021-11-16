b8aa073903a7090d184701b52c9aff96eec3d334
Mika Patterson <mknpatt21@gmail.com>
2021-11-16 01:43:50 +0000

    Created users table

---
+++ model.py
@@ -0,0 +1,17 @@
+"""Models for melon tasting scheduler app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<User ID={self.user_id} Name={self.name} Email={self.email}>"
