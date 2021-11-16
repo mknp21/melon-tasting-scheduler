495dccd57a6f34ac51b3f25403fcc44a9696b749
Mika Patterson <mknpatt21@gmail.com>
2021-11-16 01:56:45 +0000

    Created reservations table and connect to db fxn

--- model.py
+++ model.py
@@ -13,5 +13,41 @@ class User(db.Model):
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
 
+    # reservations = a list of Reservation objects
+
     def __repr__(self):
         return f"<User ID={self.user_id} Name={self.name} Email={self.email}>"
+
+class Reservation(db.Model):
+    """A restaurant."""
+
+    __tablename__ = "reservations"
+
+    res_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    details = db.Column(db.DateTime)
+
+    user = db.relationship("User", backref="reservations")
+
+    def __repr__(self):
+        return f"<Res ID={self.res_id} User={self.user_id} Details={self.details}>"
+
+def connect_to_db(flask_app, db_uri="postgresql:///rezzies", echo=True):
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
+    flask_app.config["SQLALCHEMY_ECHO"] = echo
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print("Connected to the db!")
+
+
+if __name__ == "__main__":
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
